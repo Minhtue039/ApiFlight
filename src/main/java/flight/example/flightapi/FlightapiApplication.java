1b5ebb015d430d314a4ae9eebf2b2d591180c33d
minhtuenguyen10 <minhtuelt0309@gmail.com>
2026-01-21 09:07:12 +0700

    Disable DataSource and Hibernate auto-configuration to run without DB

--- src/main/java/flight/example/flightapi/FlightapiApplication.java
+++ src/main/java/flight/example/flightapi/FlightapiApplication.java
@@ -2,12 +2,17 @@ package flight.example.flightapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAsync
 @EnableScheduling
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
+})
 public class FlightapiApplication {
 
 	public static void main(String[] args) {
